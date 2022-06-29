1657e1b05176a4d567dd7132347138f32c9756f7
minsu <jminsu084@gmail.com>
2022-06-29 21:11:44 +0900

    리포지토리 인터페이스, 구현 클래스 생성 및 구현

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // null이 반환될 것 같으면Optional로 감싸면 된다. 자세한건 뒤에서
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream() // filter부터는 for문처럼 돌리는 것이다.
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 하나라도 찾으면 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
