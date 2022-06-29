039a55c359792054d7737230b03544c98e2415e5
minsu <jminsu084@gmail.com>
2022-06-30 01:23:02 +0900

    회원 서비스 테스트 코드 작성 및 완료

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,15 +2,19 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    
+    private final MemberRepository memberRepository;
+
+    // new를 사용하지 않기 위해서 변경한 코드 + 같은 레파지토리를 테스트하기 위함
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     // 회원 가입
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원이 있으면 안됨
