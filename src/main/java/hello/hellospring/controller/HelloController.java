4d8a609061d1c1898b640e27e730d965dadd99ae
minsu <jminsu084@gmail.com>
2022-06-29 19:21:09 +0900

    MVC와 템플릿 엔진까지 듣기 완료

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -14,6 +14,9 @@ public class HelloController {
         return "hello";
     }
 
+    // 오류가 나는 이유는 name 값때문이다. 이를 해결하기 위한 방법 중 하나로
+    // @RequestParam(value = "name", required = false)하면 됨.
+    // ctl + p 유용하게 사용되니까 기억해두기
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         // 파라미터로 넘겨온 name을 넘긴다.
