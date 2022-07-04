2a3aae5307e458c0b5920cbfd087e63fe457a413
minsu <jminsu084@gmail.com>
2022-07-04 11:31:33 +0900

    자바 코드로 직접 스프링 빈 등록하기 위해 Autowired, Service, Repository 어노테이션 제거

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,6 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -12,7 +11,7 @@ public class MemberController {
 
     // 2. 생성자 만들기
     // 3. Autowired 어노테이션 달아주기 => 그러면 연결된다.
-    @Autowired // 생성자에 Autowired 어노테이션이 붙어있으면 스프링이 스프링컨테이너에 등록되어있는 memberService를 등록시켜준다
+    // @Autowired // 생성자에 Autowired 어노테이션이 붙어있으면 스프링이 스프링컨테이너에 등록되어있는 memberService를 등록시켜준다
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
