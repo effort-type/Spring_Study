01b9dc71acc3d6740e8e01c83735ac99b9465da5
minsu <jminsu084@gmail.com>
2022-07-04 12:18:39 +0900

    회원 웹 기능 : 등록

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,15 +1,11 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.service.MemberService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
+    /*@Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
@@ -17,6 +13,6 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
-    }
+    }*/
 
 }
