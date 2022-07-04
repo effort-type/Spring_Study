7ca19bbf11b2690a00d68e9578c611f25b37e7c4
minsu <jminsu084@gmail.com>
2022-07-04 11:25:44 +0900

    자동 의존관계 설정, Autowired 어노테이션 사용

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,19 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    // 1. 선언
+    private final MemberService memberService;
+
+    // 2. 생성자 만들기
+    // 3. Autowired 어노테이션 달아주기 => 그러면 연결된다.
+    @Autowired // 생성자에 Autowired 어노테이션이 붙어있으면 스프링이 스프링컨테이너에 등록되어있는 memberService를 등록시켜준다
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,15 +2,19 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service // 스프링 컨테이너에 등록해주는 어노테이션
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
     // new를 사용하지 않기 위해서 변경한 코드 + 같은 레파지토리를 테스트하기 위함
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
